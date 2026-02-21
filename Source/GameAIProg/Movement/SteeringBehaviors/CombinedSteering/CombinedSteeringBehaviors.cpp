11315874479a39748f2ebd8abb0743ce308c4512
Damian Ziolo <damian.ziolo@student.howest.be>
2026-02-21 12:08:36 +0100

    Normalize and fix BlendedSteering calculations
    Refactors BlendedSteering::CalculateSteering to use a local result variable, skip invalid behaviors, and compute per-behavior steering before combining. Each behavior's linear output is normalized prior to weighting to prevent high-magnitude behaviors (e.g. Seek) from dominating

--- Source/GameAIProg/Movement/SteeringBehaviors/CombinedSteering/CombinedSteeringBehaviors.cpp
+++ Source/GameAIProg/Movement/SteeringBehaviors/CombinedSteering/CombinedSteeringBehaviors.cpp
@@ -11,41 +11,51 @@ BlendedSteering::BlendedSteering(const std::vector<WeightedBehavior>& WeightedBe
 //BLENDED STEERING
 SteeringOutput BlendedSteering::CalculateSteering(float DeltaT, ASteeringAgent& Agent)
 {
-	SteeringOutput BlendedSteering = {};
+	//Result
+	SteeringOutput result = {};
 	// TODO: Calculate the weighted average steeringbehavior
 	
 	FVector2D sumLinear = FVector2D::ZeroVector;
 	float sumAngular = 0.f;
 	float sumWeights = 0.f;
 
+	//for each behaviour, calculate steering and add it do weight
 	for (const WeightedBehavior& behavior : WeightedBehaviors)
 	{
+		//skip empty/invalid behaviours
 		if (!behavior.pBehavior || behavior.Weight <= 0.f)
 		{
 			continue;
 		}
-		
+		//Calculate steering of THIS behaviour(Seek, Wander, etc)
 		const SteeringOutput steering = behavior.pBehavior->CalculateSteering(DeltaT, Agent);
 		
-		sumLinear += steering.LinearVelocity * behavior.Weight;
+		//We're adding steering to sum
+		const FVector2D dir = steering.LinearVelocity.GetSafeNormal(); //we need to take normal becuse seek vector is biger than wander and that's why seek is ,,stronger"
+		sumLinear += dir * behavior.Weight;
 		sumAngular += steering.AngularVelocity * behavior.Weight;
+		
+		//We sum weights
 		sumWeights += behavior.Weight;
 	}
 	
+	
+	//if we have any weights, we divide by it
 	if (sumWeights > 0.f)
 	{
-		BlendedSteering.LinearVelocity = sumLinear / sumWeights;
-		BlendedSteering.AngularVelocity = sumAngular / sumWeights;
+		const FVector2D resultDir = sumLinear / sumWeights;
+		result.LinearVelocity = resultDir.GetSafeNormal() * Agent.GetMaxLinearSpeed();
+		result.AngularVelocity = sumAngular / sumWeights;
 	}
 	// TODO: Add debug drawing
 		const FVector start = FVector(Agent.GetPosition(), 0.f);
-		const FVector2D dir2D = BlendedSteering.LinearVelocity.GetSafeNormal();
+		const FVector2D dir2D = result.LinearVelocity.GetSafeNormal();
 		const FVector end = start + FVector(dir2D, 0.f) * 200.f;
 
 		DrawDebugLine(Agent.GetWorld(), start, end, FColor::Magenta, false, 0.f, 0, 2.f);
 
 	
-	return BlendedSteering;
+	return result;
 }
 
 float* BlendedSteering::GetWeight(ISteeringBehavior* const SteeringBehavior)
