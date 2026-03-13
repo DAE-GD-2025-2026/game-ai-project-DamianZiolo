5f004588b73210f032e4891a89fd08393b4dcb36
DamianZiolo <damian.ziolo@student.howest.be>
2026-03-13 18:59:39 +0100

    Implemented BFS
    Implemented BFS code using pseudocode from slides

--- Source/GameAIProg/GraphTheory/Algorithms/BFS.cpp
+++ Source/GameAIProg/GraphTheory/Algorithms/BFS.cpp
@@ -16,5 +16,66 @@ BFS::BFS(Graph* const pGraph)
 std::vector<Node*> BFS::FindPath(Node* const pStartNode, Node* const pDestinationNode) const
 {
 	std::vector<Node*> path;
+	std::queue<Node*> queue;
+	std::vector<Node*> visited;
+	std::map<Node*, Node*> parent;
+
+	queue.push(pStartNode);
+	visited.push_back(pStartNode);
+
+	Node* currentNode = nullptr;
+
+	while (!queue.empty())
+	{
+		currentNode = queue.front();
+		queue.pop();
+
+		if (currentNode == pDestinationNode)
+		{
+			break;
+		}
+
+		auto connections = pGraph->FindConnectionsFrom(currentNode->GetId());
+
+		for (auto connection : connections)
+		{
+			auto nextNode = pGraph->GetNode(connection->GetToId()).get();
+
+			bool alreadyVisited = false;
+
+			for (auto node : visited)
+			{
+				if (node == nextNode)
+				{
+					alreadyVisited = true;
+					break;
+				}
+			}
+
+			if (alreadyVisited)
+				continue;
+
+			visited.push_back(nextNode);
+			parent[nextNode] = currentNode;
+			queue.push(nextNode);
+		}
+	}
+
+	// reconstruct path
+	if (currentNode == pDestinationNode)
+	{
+		Node* node = pDestinationNode;
+
+		while (node != pStartNode)
+		{
+			path.push_back(node);
+			node = parent[node];
+		}
+
+		path.push_back(pStartNode);
+
+		std::reverse(path.begin(), path.end());
+	}
+
 	return path;
 }
