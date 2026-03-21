fce5ca31656f7dac55c2e0705c0747e7f224a541
DamianZiolo <damian.ziolo@student.howest.be>
2026-03-21 13:27:02 +0100

    Implemented CreateNavigationGraph method in NavGraph
    I created a method that check all edged of the mesh and create nodes in the middle of common edge for 2 triangles. Then I create connection inside triangles and set correct weight(cost) to the connections with SetConnecionCostsToDistances method.

--- Source/GameAIProg/Shared/Graph/NavGraph/NavGraph.cpp
+++ Source/GameAIProg/Shared/Graph/NavGraph/NavGraph.cpp
@@ -49,11 +49,72 @@ int GameAI::NavGraph::GetNodeIdFromEdgeIndex(int EdgeIdx) const
 void GameAI::NavGraph::CreateNavigationGraph()
 {
 	//1. Go over all the edges of the navigation mesh and create nodes
+	auto const& edges = pNavPoly->GetEdges();
+	auto const& triangles = pNavPoly->GetTriangles();
+	for (int EdgeIndex = 0; EdgeIndex < edges.size(); ++EdgeIndex)
+	{
+		const auto& currentEdge = edges[EdgeIndex];
+		int triangleCount = 0;
+		for (auto const & triangle : triangles)
+		{
+			if (triangle.HasEdge(currentEdge))
+			{
+				triangleCount++;
+			}
+		}
+		
+		if (triangleCount == 2)
+		{
 			// Create node here
-
-	//2. Create connections now that every node is created	
+			FVector p1 = currentEdge.GetP1(*pNavPoly);
+			FVector p2 = currentEdge.GetP2(*pNavPoly);
+			FVector pointMiddle = (p1 + p2) / 2;
+			
+			auto newNode = std::make_unique<NavGraphNode>(FVector2D(pointMiddle),EdgeIndex);
+			AddNode(std::move(newNode) );
+		}
+		
+	}
+	
+	for (auto const & triangle : triangles)
+	{
+		auto triangleEdges = triangle.GetEdges();
+		std::vector<int> nodeIndexs;
+		for (auto const & edge : triangleEdges)
+		{
+			auto edgeIdex = pNavPoly->FindEdgeIndex(edge);
+			if (!edgeIdex.has_value())
+			{
+				continue;
+			}
+			
+			int nodeId = GetNodeIdFromEdgeIndex(edgeIdex.value());
+			if (nodeId != Graphs::InvalidNodeId)
+			{
+				nodeIndexs.emplace_back(nodeId);
+			}
+		}
+		//2. Create connections now that every node is created	
 		//2 valid nodes -> 1 connection
 		//3 valid nodes -> 3 connections
-		
+		if (nodeIndexs.size() == 2)
+		{
+			AddConnection(nodeIndexs[0], nodeIndexs[1]);
+			AddConnection(nodeIndexs[1], nodeIndexs[0]);
+		}
+		else if (nodeIndexs.size() == 3)
+		{
+			AddConnection(nodeIndexs[0], nodeIndexs[1]);
+			AddConnection(nodeIndexs[1], nodeIndexs[0]);
+			
+			AddConnection(nodeIndexs[2], nodeIndexs[1]);
+			AddConnection(nodeIndexs[1], nodeIndexs[2]);
+			
+			AddConnection(nodeIndexs[0], nodeIndexs[2]);
+			AddConnection(nodeIndexs[2], nodeIndexs[0]);
+			
+		}
+	}
 	//3. Set the connections cost to the actual distance
+	SetConnectionCostsToDistances();
 }
